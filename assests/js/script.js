fef921f9c28831d0cc52b2500b1fd451a52d06bd
LP-user1 <akashbhuvi3@gmail.com>
2023-01-22 17:20:36 +0530

    Feature : Added nav-fixed while scroll

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,8 +1,14 @@
+// Dynamic Nav Toggle
 const navBtn = document.querySelector(".nav-toggler");
 const navIcon = document.querySelectorAll(".nav-icon");
 const navMenu = document.querySelector(".header--menu");
-const menuLink = document.querySelectorAll(".menu-link");
+const menuLinkLi = document.querySelectorAll(".menu-link");
+const menuLink = document.querySelectorAll("li.menu-link a");
 
+// Fixed Navbar while Scroll
+const header = document.querySelector(".header");
+
+// Toggle button Event Listener
 navBtn.addEventListener("click", () => {
   navMenu.classList.toggle("show");
   navIcon.forEach((icon) => {
@@ -10,18 +16,38 @@ navBtn.addEventListener("click", () => {
   });
 });
 
-// menuLink.forEach((link) => {
-//   link.addEventListener("click", () => {
-//     navMenu.classList.toggle("show");
-//     navIcon.forEach((icon) => {
-//       icon.classList.toggle("hide");
-//     });
-//     let active = link.nextElementSibling.classList.contains("active");
-//     console.log(active);
-//     if (!active) {
-//       link.nextElementSibling.classList.add("active");
-//     } else {
-//       link.nextElementSibling.classList.remove("active");
-//     }
-//   });
-// });
+// Nav menu link Event Listener
+menuLinkLi.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    let active = e.target.classList.contains("active");
+    menuLink.forEach((e) => {
+      e.classList.remove("active");
+    });
+    if (!active) {
+      e.target.classList.add("active");
+    } else {
+      e.target.classList.remove("active");
+    }
+    navMenu.classList.toggle("show");
+    navIcon.forEach((icon) => {
+      icon.classList.toggle("hide");
+    });
+  });
+});
+
+// Navbar fixed while scroll function
+function navFixed() {
+  let navOffsetTop = header.clientHeight + 60;
+
+  window.addEventListener("scroll", () => {
+    let scroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scroll >= navOffsetTop) {
+      header.classList.add("nav-fixed");
+    } else {
+      header.classList.remove("nav-fixed");
+    }
+  });
+}
+
+navFixed();
